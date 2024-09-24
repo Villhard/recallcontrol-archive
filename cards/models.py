70f289cfe8290ec67555bf78fe3cb8e73ebad432
Victor Ryabov <victorwillhard@gmail.com>
2024-09-24 18:37:44 +0300

    feat: add created_at and updated_at fields to Card model

--- cards/models.py
+++ cards/models.py
@@ -4,6 +4,8 @@ from django.db import models
 class Card(models.Model):
     front = models.TextField()
     back = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
     is_active = models.BooleanField(default=True)
 
     def __str__(self):
