15e08ff2e5bcf8e00a514019709b993db98ac020
Victor Ryabov <victorwillhard@gmail.com>
2024-09-29 14:11:35 +0300

    refactor: update urls.py to include namespace for cards and users apps

--- recallcontrol/urls.py
+++ recallcontrol/urls.py
@@ -3,6 +3,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", include("cards.urls")),
-    path("users/", include("users.urls")),
+    path("", include("cards.urls", namespace="cards")),
+    path("users/", include("users.urls", namespace="users")),
 ]
