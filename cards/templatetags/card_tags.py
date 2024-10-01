3adb0985b801c0b03735e8885705fe35638d2408
Victor Ryabov <victorwillhard@gmail.com>
2024-10-01 05:18:47 +0300

    refactor(card_tags.py): Update card_for_studying template tag to filter cards by owner

--- cards/templatetags/card_tags.py
+++ cards/templatetags/card_tags.py
@@ -5,6 +5,8 @@ from cards.models import Card
 register = template.Library()
 
 
-@register.simple_tag
-def card_for_studying():
-    return Card.study_objects.count()
+@register.simple_tag(takes_context=True)
+def card_for_studying(context):
+    user = context["request"].user
+    if user.is_authenticated:
+        return Card.study_objects.filter(owner=user).count()
