98fe76322319e67b7f251b199fc8456cb91f1e3c
Victor Ryabov <victorwillhard@gmail.com>
2024-10-01 05:21:22 +0300

    refactor(views.py): Update CardListView and CardUpdateView to filter cards by owner and check permission

--- cards/views.py
+++ cards/views.py
@@ -1,47 +1,72 @@
-from django.views.generic import (
-    TemplateView,
-    ListView,
-    CreateView,
-    UpdateView,
-    DeleteView,
-)
-from django.views import View
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.core.exceptions import PermissionDenied
 from django.shortcuts import redirect, render
 from django.utils import timezone
-from cards.models import Card
+from django.views import View
+from django.views.generic import (
+    CreateView,
+    DeleteView,
+    ListView,
+    TemplateView,
+    UpdateView,
+)
+
 from cards.forms import CardForm
+from cards.models import Card
 
 
 class IndexView(TemplateView):
     template_name = "cards/index.html"
 
 
-class CardListView(ListView):
+class CardListView(LoginRequiredMixin, ListView):
     model = Card
-    queryset = Card.objects.all()
     context_object_name = "cards"
     template_name = "cards/card_list.html"
 
+    def get_queryset(self):
+        return Card.objects.filter(owner=self.request.user)
 
-class CardCreateView(CreateView):
+
+class CardCreateView(LoginRequiredMixin, CreateView):
     model = Card
     form_class = CardForm
     template_name = "cards/card_form.html"
     success_url = "/cards/"
     extra_context = {"title": "Create Card"}
 
+    def form_valid(self, form):
+        form.instance.owner = self.request.user
+        return super().form_valid(form)
 
-class CardUpdateView(UpdateView):
+
+class CardUpdateView(LoginRequiredMixin, UpdateView):
     model = Card
     form_class = CardForm
     template_name = "cards/card_form.html"
     success_url = "/cards/"
     extra_context = {"title": "Update Card"}
 
+    def get_object(self, queryset=None):
+        obj = super().get_object(queryset)
+        if obj.owner != self.request.user:
+            raise PermissionDenied("You do not have permission to edit this card")
+        return obj
 
-class CardDeleteView(DeleteView):
+    def form_valid(self, form):
+        form.instance.owner = self.request.user
+        return super().form_valid(form)
+
+
+class CardDeleteView(LoginRequiredMixin, DeleteView):
     model = Card
     success_url = "/cards/"
+
+    def get_object(self, queryset=None):
+        obj = super().get_object(queryset)
+        if obj.owner != self.request.user:
+            raise PermissionDenied("You do not have permission to delete this card")
+        return obj
 
     def get(self, request, *args, **kwargs):
         return self.post(request, *args, **kwargs)
@@ -52,9 +77,9 @@ class CardDeleteView(DeleteView):
         return redirect(self.success_url)
 
 
-class CardStudyView(View):
+class CardStudyView(LoginRequiredMixin, View):
     def get(self, request):
-        card = Card.study_objects.first()
+        card = Card.study_objects.filter(owner=request.user).first()
         return render(request, "cards/card_study.html", {"card": card})
 
     def post(self, request):
