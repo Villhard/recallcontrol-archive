25c253fee5b260ab898e1b14c2387b9be57f8b27
Victor Ryabov <victorwillhard@gmail.com>
2024-10-02 03:28:35 +0300

    refactor(templates/registration/register.html): Update registration form to use Russian language

--- users/forms.py
+++ users/forms.py
@@ -1,10 +1,15 @@
 from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import UserCreationForm
+from django import forms
 
 User = get_user_model()
 
 
 class UserRegisterForm(UserCreationForm):
+    username = forms.CharField(label="Имя пользователя", max_length=30)
+    password1 = forms.CharField(label="Пароль", widget=forms.PasswordInput)
+    password2 = forms.CharField(label="Подтверждение пароля", widget=forms.PasswordInput)
+
     class Meta:
         model = User
         fields = ["username", "password1", "password2"]
