d707bddb848f4619338b9d7d52689ffeed0e6e34
Victor Ryabov <victorwillhard@gmail.com>
2024-10-19 14:48:38 +0300

    feat: add URL pattern for API authentication using Django Rest Framework.

--- backend/recallcontrol/urls.py
+++ backend/recallcontrol/urls.py
@@ -6,6 +6,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/v1/", include("api.urls")),
+    path("api-auth/", include("rest_framework.urls")),
 ]
 
 if settings.DEBUG:
