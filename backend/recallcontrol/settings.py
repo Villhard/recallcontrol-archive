db079ef952d52d93e5926f0d2cc37507c5874b54
Victor Ryabov <victorwillhard@gmail.com>
2024-10-19 09:24:07 +0300

    chore(settings): remove unused settings: LOGIN_REDIRECT_URL, LOGOUT_REDIRECT_URL, SESSION_COOKIE_AGE, STATICFILES_DIRS

--- backend/recallcontrol/settings.py
+++ backend/recallcontrol/settings.py
@@ -10,11 +10,6 @@ ALLOWED_HOSTS = ["*"]
 
 AUTH_USER_MODEL = "users.CustomUser"
 
-LOGIN_REDIRECT_URL = "cards:index"
-LOGOUT_REDIRECT_URL = "cards:index"
-
-SESSION_COOKIE_AGE = 60 * 60 * 24 * 30
-
 INSTALLED_APPS = [
     "django.contrib.admin",
     "django.contrib.auth",
@@ -89,7 +84,6 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_URL = "/static/"
-STATICFILES_DIRS = [BASE_DIR / "static"]
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
 MEDIA_URL = "/media/"
