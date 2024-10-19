8e96a4171f506e133c2593ee2ad6930cf04dc961
Victor Ryabov <victorwillhard@gmail.com>
2024-10-19 07:58:06 +0300

    feat(project): start global project redesign

--- backend/api/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from cards.models import Card
-
-
-class CardSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Card
-        fields = ["id", "front", "back"]
--- backend/api/views.py
+++
@@ -1,8 +0,0 @@
-from rest_framework.viewsets import ModelViewSet
-from cards.models import Card
-from api.serializers import CardSerializer
-
-
-class CardViewSet(ModelViewSet):
-    queryset = Card.objects.all()
-    serializer_class = CardSerializer
