0ad2e734151bce4830ada1b818e7f3cf9e503ca8
Victor Ryabov <victorwillhard@gmail.com>
2024-10-19 14:07:09 +0300

    feat: update API URL prefix and add REST framework settings

--- backend/recallcontrol/settings.py
+++ backend/recallcontrol/settings.py
@@ -33,6 +33,12 @@ MIDDLEWARE = [
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
+REST_FRAMEWORK = {
+    "DEFAULT_PERMISSION_CLASSES": ["rest_framework.permissions.IsAuthenticatedOrReadOnly"],
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+    "PAGE_SIZE": 20,
+}
+
 ROOT_URLCONF = "recallcontrol.urls"
 
 TEMPLATES = [
